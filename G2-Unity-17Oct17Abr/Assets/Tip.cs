f39b05859677f1361d4544bb71aa84a9d188199e
9lo01 <83479426+9lo01@users.noreply.github.com>
2023-01-10 11:30:49 -0600

    main - tutorial part 1
    de como escribimos los primeros 2 pasos de tutorial, con su script.

--- G2-Unity-17Oct17Abr/Assets/Tip.cs
+++ G2-Unity-17Oct17Abr/Assets/Tip.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Tip : MonoBehaviour
 {
+    public string TextInScreen;
+    private TMP_Text TextUI;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TextUI = GetComponent<TMP_Text>();
+        TextUI.alignment = TextAlignmentOptions.Center;
+        TextUI.alignment = TextAlignmentOptions.Midline;
+        TextUI.text = TextInScreen;
     }
 
     // Update is called once per frame
